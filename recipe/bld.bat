55678742a77ce9e83c9d64912377d27ae8f1bc95
Klaus Zimmermann <klaus.zimmermann@smhi.se>
2023-06-09 16:05:25 +0200

    Add Python to Windows build

--- recipe/bld.bat
+++ recipe/bld.bat
@@ -9,6 +9,7 @@ cmake %SRC_DIR% -G "NMake Makefiles" ^
                 -DBUILD_SHARED_LIBS=ON ^
                 -DBUILD_EXAMPLES=OFF ^
                 -DCMAKE_WINDOWS_EXPORT_ALL_SYMBOLS=TRUE ^
+                -DWITH_PYTHON=TRUE ^
                 -UGTEST_ROOT
 if errorlevel 1 exit 1
 
@@ -22,3 +23,5 @@ copy %PREFIX%\Library\bin\s2.dll %PREFIX%\Library\bin\libs2.dll
 if errorlevel 1 exit 1
 copy %PREFIX%\Library\lib\s2.lib %PREFIX%\Library\lib\libs2.lib
 if errorlevel 1 exit 1
+
+%PYTHON%  -m pip install . --no-deps -vv
